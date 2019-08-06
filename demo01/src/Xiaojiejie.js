7b752ead1732d61301c683a9f5ba3ccdbc026879
charbo23 <charbo233@qq.com>
2019-08-06 15:43:25 +0800

    生命周期结束

--- demo01/src/Xiaojiejie.js
+++ demo01/src/Xiaojiejie.js
@@ -1,52 +1,55 @@
-import React,{Component,Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import shortid from 'shortid';
 import './style.css';
 import XiaojiejieItem from "./XiaojiejieItem";
 
 class Xiaojiejie extends Component {
-    constructor(props){
+    //生命周期函数指在某一个时刻组件会自动调用执行的函数
+    constructor(props) {
         super(props);
         this.inputChange = this.inputChange.bind(this);
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
         this.onKeyUp = this.onKeyUp.bind(this);
         this.state = {
-            inputValue:'',
-            list:[
-                {id:shortid.generate(),value:'基础按摩'},
-                {id:shortid.generate(),value:'精油推背'}
+            inputValue: '',
+            list: [
+                { id: shortid.generate(), value: '基础按摩' },
+                { id: shortid.generate(), value: '精油推背' }
             ]
         }
     }
+
+
     render() {
         return (
             <Fragment>
                 <div>
                     {/* for改为htmlFor,class改成className */}
                     <label htmlFor="serviceName">增加服务：</label>
-                    <input 
-                        id="serviceName" 
-                        className="input" 
-                        value={this.state.inputValue} 
+                    <input
+                        id="serviceName"
+                        className="input"
+                        value={this.state.inputValue}
                         onChange={this.inputChange}
                         onKeyUp={this.onKeyUp}
-                        ref={(inputElement)=>{this.input=inputElement}}
+                        ref={(inputElement) => { this.input = inputElement }}
                     />
                     <button onClick={this.addItem}>增加服务</button>
                 </div>
-                <ul ref={(ulElement)=>{this.ul=ulElement}}>
+                <ul ref={(ulElement) => { this.ul = ulElement }}>
                     {
-                        this.state.list.map((item,index)=>{
+                        this.state.list.map((item, index) => {
                             return (
-                            // <li key={item.id}>
-                            //     <span dangerouslySetInnerHTML={{__html:item.value}}></span>
-                            //     <button data-id={item.id} onClick={this.deleteItem}>删除</button>
-                            // </li> 
-                                <XiaojiejieItem 
-                                    key={item.id} 
-                                    content={item} 
-                                    deleteItem = {this.deleteItem}
-                                    // avname="Jane Doe"
+                                // <li key={item.id}>
+                                //     <span dangerouslySetInnerHTML={{__html:item.value}}></span>
+                                //     <button data-id={item.id} onClick={this.deleteItem}>删除</button>
+                                // </li> 
+                                <XiaojiejieItem
+                                    key={item.id}
+                                    content={item}
+                                    deleteItem={this.deleteItem}
+                                // avname="Jane Doe"
                                 />
                             )
                         })
@@ -56,34 +59,32 @@ class Xiaojiejie extends Component {
         );
     }
 
-    inputChange(e){
+    inputChange(e) {
         // console.log(e.target.value);
         // this.state.inputValue=e.target.value;
         this.setState({
             inputValue: this.input.value
         })
     }
-    onKeyUp(e){
-        if(e.keyCode === 13) {
+    onKeyUp(e) {
+        if (e.keyCode === 13) {
             this.addItem()
         }
     }
-    addItem(){
-        if(!this.state.inputValue){
+    async addItem() {
+        if (!this.state.inputValue) {
             return;
         }
-        
-        //setState为异步方法
-        this.setState({
+        //setState为异步方法,可用async/await或者回调函数
+        await this.setState({
             //...this.state.list相当于数组中原来的元素
-            list:[...this.state.list,{id:shortid.generate(),value:this.state.inputValue}],
-            inputValue:''
-        },()=>{
-            console.log(this.ul.querySelectorAll('li').length);
+            list: [...this.state.list, { id: shortid.generate(), value: this.state.inputValue }],
+            inputValue: ''
         })
+        // console.log(this.ul.querySelectorAll('li').length);
         
     }
-    
+
     // deleteItem(e){
     //    let index = e.target.getAttribute('data-id');
     //    //这样可以避免直接操作 this.state.list
@@ -93,13 +94,13 @@ class Xiaojiejie extends Component {
     //        list: list
     //    });
     // }
-    deleteItem(itemId){
+    deleteItem(itemId) {
         let list = [...this.state.list];
         list = list.filter((listItem) => listItem.id !== itemId);
         this.setState({
             list: list
         });
-     }
+    }
 }
 
 export default Xiaojiejie;
--- demo01/src/XiaojiejieItem.js
+++ demo01/src/XiaojiejieItem.js
@@ -2,40 +2,53 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class XiaojiejieItem extends Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
     }
-    
-    render() { 
-        return ( 
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.content !== this.props.content;
+    }
+
+    //组件第一次出现在DOM中，函数不会执行
+    //如果已经存在于DOM中，即发生变化才会执行
+    // componentWillReceiveProps(nextProps) {
+    //     console.log(nextProps);
+    // }
+    // componentWillUnmount() {
+    //     console.log("componentWillReceiveProps——子组件被删除");
+    // }
+    render() {
+        console.log("Child render");
+        return (
             <li>
                 <span>
                     {this.props.avname}-为你做-{this.props.content.value}
                 </span>
                 <button data-id={this.props.content.id} onClick={this.handleClick}>删除</button>
             </li>
-         );
+        );
     }
 
-    handleClick(){
+    handleClick() {
         this.props.deleteItem(this.props.content.id);
-        
+
     }
 
 }
 
-XiaojiejieItem.propTypes={
-    avname:PropTypes.string.isRequired,
-    content:PropTypes.object,
-    index:PropTypes.number,
-    deleteItem:PropTypes.func
+XiaojiejieItem.propTypes = {
+    avname: PropTypes.string.isRequired,
+    content: PropTypes.object,
+    index: PropTypes.number,
+    deleteItem: PropTypes.func
 }
 
-XiaojiejieItem.defaultProps={
-    avname:'Jane Doe',
-    
+XiaojiejieItem.defaultProps = {
+    avname: 'Jane Doe',
+
 }
 
 export default XiaojiejieItem;
